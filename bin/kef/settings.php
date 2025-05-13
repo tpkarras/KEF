831c570553e680bee758776150bedea3591aac27
tpkarras <taylorkarras@gmail.com>
2025-05-13 14:54:47 -0700

    Update settings.php

--- bin/kef/settings.php
+++ bin/kef/settings.php
@@ -4,6 +4,7 @@ namespace tpkarras\KEF;
 class Settings{
 
 public const MIN_BYTES = 2;
+public const MAX_BYTES = 1024000000;
 public const DEFAULT_BYTE_RANGE = 16384;
 public const DEFAULT_BUFFER_SIZE = 2560000;
 
