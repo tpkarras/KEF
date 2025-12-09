fdfd4b198df26ce7f25116c2ae493ecee74bac76
tpkarras <taylorkarras@gmail.com>
2025-12-08 19:35:48 -0800

    indenting code for increased legibility

--- bin/kef/ciphers.php
+++ bin/kef/ciphers.php
@@ -9,171 +9,171 @@ public static function convertCipher(string|array $cipher){
 $cipher_methods = ["aes", "aria", "camellia", "chacha20", "des", "des3", "sm4"];
 
 $cipher_options = [["string" => "128", "compatible" => [0, 1, 2]],
-["string" => "192", "compatible" => [0, 1, 2]],
-["string" => "256", "compatible" => [0, 1, 2]],
-["string" => "cbc", "compatible" => [0, 1, 2, 4, 6]],
-["string" => "ccm", "compatible" => [0, 1]],
-["string" => "cfb", "compatible" => [0, 1, 2, 4, 6]],
-["string" => "cfb1", "compatible" => [0, 1, 2, 4]],
-["string" => "cfb8", "compatible" => [0, 1, 2, 4]],
-["string" => "ctr", "compatible" => [0, 1, 2, 6]],
-["string" => "cts", "compatible" => [0, 2]],
-["string" => "ecb", "compatible" => [0, 1, 2, 4, 6]],
-["string" => "ede", "compatible" => [4]],
-["string" => "ede3", "compatible" => [4]],
-["string" => "gcm", "compatible" => [0, 1]],
-["string" => "inv", "compatible" => [0]],
-["string" => "ocb", "compatible" => [0]],
-["string" => "ofb", "compatible" => [0, 1, 2, 4, 6]],
-["string" => "pad", "compatible" => [0]],
-["string" => "poly1305", "compatible" => [3]],
-["string" => "siv", "compatible" => [0]],
-["string" => "wrap", "compatible" => [0, 4, 5]],
-["string" => "xts", "compatible" => [0]]];
+	["string" => "192", "compatible" => [0, 1, 2]],
+	["string" => "256", "compatible" => [0, 1, 2]],
+	["string" => "cbc", "compatible" => [0, 1, 2, 4, 6]],
+	["string" => "ccm", "compatible" => [0, 1]],
+	["string" => "cfb", "compatible" => [0, 1, 2, 4, 6]],
+	["string" => "cfb1", "compatible" => [0, 1, 2, 4]],
+	["string" => "cfb8", "compatible" => [0, 1, 2, 4]],
+	["string" => "ctr", "compatible" => [0, 1, 2, 6]],
+	["string" => "cts", "compatible" => [0, 2]],
+	["string" => "ecb", "compatible" => [0, 1, 2, 4, 6]],
+	["string" => "ede", "compatible" => [4]],
+	["string" => "ede3", "compatible" => [4]],
+	["string" => "gcm", "compatible" => [0, 1]],
+	["string" => "inv", "compatible" => [0]],
+	["string" => "ocb", "compatible" => [0]],
+	["string" => "ofb", "compatible" => [0, 1, 2, 4, 6]],
+	["string" => "pad", "compatible" => [0]],
+	["string" => "poly1305", "compatible" => [3]],
+	["string" => "siv", "compatible" => [0]],
+	["string" => "wrap", "compatible" => [0, 4, 5]],
+	["string" => "xts", "compatible" => [0]]];
 
-if(empty($cipher)){
+	if(empty($cipher)){
 
-throw new KEFException(4, 4);
+	throw new KEFException(4, 4);
 
-}
+	}
 
-if(is_string($cipher)){
+	if(is_string($cipher)){
 
-if(array_search($cipher, openssl_get_cipher_methods()) === false){
+		if(array_search($cipher, openssl_get_cipher_methods()) === false){
 
-throw new KEFException(4, 5);
+		throw new KEFException(4, 5);
 
-}
+		}
 
-$cipher = explode("-", $cipher);
+	$cipher = explode("-", $cipher);
 
-foreach($cipher as $k => $v){
+	foreach($cipher as $k => $v){
 
-if($k === 0){
+		if($k === 0){
 
-$v = array_search($v, $cipher_methods);
+		$v = array_search($v, $cipher_methods);
 
-if($v === false){
+			if($v === false){
 	
-throw new KEFException(4, 5);
+			throw new KEFException(4, 5);
 
-}
+			}
 
-$cipher[$k] = $v;
+		$cipher[$k] = $v;
 
-} else {
+		} else {
 
-foreach($cipher_options as $k2 => $v2){
+	foreach($cipher_options as $k2 => $v2){
 
-if($v2["string"] === $v){
+			if($v2["string"] === $v){
 
-if(array_search(reset($cipher), $v2["compatible"]) === false){
+				if(array_search(reset($cipher), $v2["compatible"]) === false){
 
-throw new KEFException(4, 5);
+				throw new KEFException(4, 5);
 
-}
+				}
 
-$cipher[$k] = $k2;
-break;
-}
+			$cipher[$k] = $k2;
+			break;
 
-}
+			}
 
-}
+		}
 
-}
+		}
+
+	}
+
+	return $cipher;
+
+	} else {
+
+	foreach($cipher as $k => $v){
+
+		if(!is_int($v)){
+
+		throw new KEFException(4, 9);
+
+		}
+
+			if($k === 0){
+
+				if(!isset($cipher_methods[$v])){
+
+				throw new KEFException(4, 5);
+
+				}
+
+	$method = $v;
+	$cipher[$k] = $cipher_methods[$v];
+
+	} else {
+
+		if(!isset($cipher_options[$v])){
+
+		throw new KEFException(4, 5);
+
+		}
+
+		if(array_search($method, $cipher_options[$v]["compatible"]) === false){
+
+		throw new KEFException(4, 0);
+
+		}
+
+	$cipher[$k] = $cipher_options[$v]["string"];
+
+	}
+
+	}
+
+	$cipher = implode("-", $cipher);
+
+		if(array_search($cipher, openssl_get_cipher_methods()) === false){
+
+		throw new KEFException(4, 5);
+
+		}
 
 return $cipher;
 
-} else {
-
-foreach($cipher as $k => $v){
-
-if(!is_int($v)){
-
-throw new KEFException(4, 9);
-
-}
-
-if($k === 0){
-
-if(!isset($cipher_methods[$v])){
-
-throw new KEFException(4, 5);
-
-}
-
-$method = $v;
-$cipher[$k] = $cipher_methods[$v];
-
-} else {
-
-if(!isset($cipher_options[$v])){
-
-throw new KEFException(4, 5);
-
-}
-
-if(array_search($method, $cipher_options[$v]["compatible"]) === false){
-
-throw new KEFException(4, 0);
-
-}
-
-$cipher[$k] = $cipher_options[$v]["string"];
-
-}
-
-}
-
-$cipher = implode("-", $cipher);
-
-if(array_search($cipher, openssl_get_cipher_methods()) === false){
-
-throw new KEFException(4, 5);
-
-}
-
-return $cipher;
-
-}
+	}
 
 }
 
 //Check if cipher supports IV
 public static function supportsIV(string|array $cipher){
 
-if(empty($cipher)){
+	if(empty($cipher)){
 	
-throw new KEFException(4, 4);
+	throw new KEFException(4, 4);
 
-}
+	}
 
-if(is_array($cipher)){
+	if(is_array($cipher)){
 	
-$cipher = self::cipherConvert($cipher);
+	$cipher = self::cipherConvert($cipher);
 
-}
+	}
 
-if(array_search($cipher, openssl_get_cipher_methods()) === false){
+	if(array_search($cipher, openssl_get_cipher_methods()) === false){
 	
-throw new KEFException(4, 5);
+	throw new KEFException(4, 5);
 
-}
+	}
 
 $iv = @openssl_cipher_iv_length($cipher);
 
-if($iv == 0){
+	if($iv == 0){
 	
-return false;
+	return false;
 
-} else {
+	} else {
 	
-return $iv;
+	return $iv;
+
+	}
 
 }
 
 }
-
-}
-
