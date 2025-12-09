33a0fe91e1d1987b410d38c7804c091b030a278f
tpkarras <taylorkarras@gmail.com>
2025-12-08 20:02:44 -0800

    indenting code for increased legibility

--- bin/kef.php
+++ bin/kef.php
@@ -4,11 +4,12 @@ KEF v1.5.25 by tpkarras (https://github.com/tpkarras/KEF)
 This code is licensed under the GNU General Public License v3.0
 */
 
-if(version_compare(PHP_VERSION, "7.4.0") == -1){
+  if(version_compare(PHP_VERSION, "7.4.0") == -1){
 
-throw new Exception("Error loading KEF: PHP version is less than 7.4.0");
+  throw new Exception("Error loading KEF: PHP version is less than 7.4.0");
 
-}
+  }
+
 require("kef/settings.php");
 require("kef/characters.php");
 require("kef/ciphers.php");
